1222c63be1f9c75452649186125af131c1ec7c37
Özmen, Funda <UI513825@rwe-ag.com>
2024-05-24 09:33:25 +0300

    createcommand creration and implementation to application

--- CleanArch.Application/Services/CourseService.cs
+++ CleanArch.Application/Services/CourseService.cs
@@ -1,16 +1,28 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
 
 namespace CleanArch.Application.Services
 {
     public class CourseService : ICourseService
     {
         private readonly ICourseRespository _courseRepository;
-        public CourseService(ICourseRespository courseRepository)
+        private readonly IMediatorHandler _bus;
+        public CourseService(ICourseRespository courseRepository, IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
         }
+
+        public void CreateCourse(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(courseViewModel.Name, courseViewModel?.Desctiption, courseViewModel?.ImageUrl);
+            _bus.SendCommand(createCourseCommand);
+        }
+
         public CourseViewModel GetCourses()
         {
             return new CourseViewModel() { Courses = _courseRepository.GetCourses() };
--- CleanArch.Domain/Commands/CourseCommand.cs
+++ CleanArch.Domain/Commands/CourseCommand.cs
@@ -1,13 +1,8 @@
 ﻿using CleanArch.Domain.Core.Commands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArch.Domain.Commands
 {
-    public abstract class CourseCommand:Command
+    public abstract class CourseCommand : Command
     {
         public string Name { get; protected set; }
         public string? Desctiption { get; protected set; }
